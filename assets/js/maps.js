eb4b97ab2175c356a0f87230b46b4021a29b517a
marctell92 <marctell92@gmail.com>
2021-08-28 16:45:02 +0000

    Add belgium festival locations to map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,17 +2,17 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("belgium-map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 49.23247309150677,
+            lng: 13.28765404513377, 
         }
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
+        { lat: 51.09009918647197, lng: 4.382447618627929 },
+        { lat: 50.40098687106428, lng: 3.7701500181234677 },
+        { lat: 50.93116248896194, lng: 4.501878597680985 }
     ];
 
     var markers = locations.map(function(location, i) {
