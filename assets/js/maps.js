724d29695dd726f5cfcbf28f0160632a02f7ad3f
marctell92 <marctell92@gmail.com>
2021-08-28 17:00:10 +0000

    Add script for each country toggle function

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,5 +1,35 @@
+// BELGIUM MAP
 function initMap() {
     var map = new google.maps.Map(document.getElementById("belgium-map"), {
+        zoom: 3,
+        center: {
+            lat: 49.23247309150677,
+            lng: 13.28765404513377, 
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 51.09009918647197, lng: 4.382447618627929 },
+        { lat: 50.40098687106428, lng: 3.7701500181234677 },
+        { lat: 50.93116248896194, lng: 4.501878597680985 }
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers,
+    {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+}
+
+// CROATIA MAP
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("france-map"), {
         zoom: 3,
         center: {
             lat: 49.23247309150677,
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,34 @@
+// BELGIUM
 $(document).ready(function(){
-    $('#show').click(function() {
-      $('.festival-card').toggle("slide");
+    $('#show-belgium').click(function() {
+      $('.belgium-festival').toggle("slide");
     });
+});
+
+// CROATIA
+$(document).ready(function(){
+  $('#show-croatia').click(function() {
+    $('.croatia-festival').toggle("slide");
+  });
+});
+
+// FRANCE
+$(document).ready(function(){
+  $('#show-france').click(function() {
+    $('.france-festival').toggle("slide");
+  });
+});
+
+// NETHERLANDS
+$(document).ready(function(){
+  $('#show-netherlands').click(function() {
+    $('.netherlands-festival').toggle("slide");
+  });
+});
+
+// SPAIN
+$(document).ready(function(){
+  $('#show-spain').click(function() {
+    $('.spain-festival').toggle("slide");
+  });
 });
